936c1c345889f9fcb9afd70d180c70e7aeee06df
wesleymktd <wesleymktd@gmail.com>
2022-11-04 19:34:45 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,52 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    searchName: '',
+    isButtonDisabled: true,
+  };
+
+  validateButton = () => {
+    const { searchName } = this.state;
+    const number3 = 2;
+    const verifiq = searchName.length < number3;
+    this.setState({
+      isButtonDisabled: verifiq,
+    });
+  };
+
+  handleForm = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, this.validateButton);
+  };
+
   render() {
+    const { searchName, isButtonDisabled } = this.state;
     return (
-      <div data-testid="page-search">
-        <Header />
-      </div>
+      <>
+        <div data-testid="page-search">
+          <Header />
+        </div>
+        <div>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            placeholder="Nome do Artista"
+            name="searchName"
+            value={ searchName }
+            onChange={ this.handleForm }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ isButtonDisabled }
+          >
+            Pesquisar
+          </button>
+        </div>
+      </>
     );
   }
 }
